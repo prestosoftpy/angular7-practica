b1d888ad44d0fdac3ffb295e0ef1cd390f6c4364
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2018-12-15 12:36:45 -0300

    Cambios menores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular7-practica';
+  title = 'Practica de Angular7';
 }
